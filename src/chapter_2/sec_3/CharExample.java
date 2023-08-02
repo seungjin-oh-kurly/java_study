93cd0b38fb3971df8ec9406e853f9c06ad45023f
seungjin-oh-kurly <seungjin.oh@kurlycorp.com>
2023-08-03 01:41:18 +0900

    chapter2.3

--- src/chapter_2/sec_3/CharExample.java
+++ src/chapter_2/sec_3/CharExample.java
@@ -1,2 +1,17 @@
-package chapter_2.sec_3;public class CharExample {
+package chapter_2.sec_3;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = 65;
+
+        char c3 = '가';
+        char c4 = 44032;
+
+        char c5 = ' '; // 빈 문자는 컴페일 에러가 나서 공백(32)을 넣어줘야한다.
+        System.out.println("c1 = " + c1);
+        System.out.println("c2 = " + c2);
+        System.out.println("c3 = " + c3);
+        System.out.println("c4 = " + c4);
+    }
 }
