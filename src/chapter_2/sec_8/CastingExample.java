7ca33962f46ff9d14b4e5a44a1eddde8bdb76090
seungjin-oh-kurly <seungjin.oh@kurlycorp.com>
2023-08-03 01:42:10 +0900

    chapter2.8

--- src/chapter_2/sec_8/CastingExample.java
+++ src/chapter_2/sec_8/CastingExample.java
@@ -1,2 +1,23 @@
-package chapter_2.sec_8;public class CastingExample {
+package chapter_2.sec_8;
+
+public class CastingExample {
+    public static void main(String[] args) {
+        int var1 = 10;
+        byte var2 = (byte) var1;
+        System.out.println("var2 = " + var2);
+
+        long var3 = 300;
+        int var4 = (int) var3;
+        System.out.println(var4);
+
+
+        int var5 = 65;
+        char var6 = (char) var5;
+        System.out.println("var6 = " + var6);
+
+        double var7 = 3.14;
+        int var8 = (int) var7;
+        System.out.println("var8 = " + var8);
+
+    }
 }
