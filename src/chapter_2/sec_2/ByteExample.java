dd90d71c235a7aa9a5397eca5b7d9920b05f2ccc
seungjin-oh-kurly <seungjin.oh@kurlycorp.com>
2023-08-03 01:41:11 +0900

    chapter2.2

--- src/chapter_2/sec_2/ByteExample.java
+++ src/chapter_2/sec_2/ByteExample.java
@@ -1,17 +1,20 @@
 package chapter_2.sec_2;
 
-public class IntegerLiteralExample {
+public class ByteExample {
     public static void main(String[] args) {
-        int var1 = 0b1011;
-        int var2 = 0206;
-        int var3 = 365;
-        int var4 = 0xb3;
+        byte var1 = -128;
+        byte var2 = -30;
+        byte var3 = 0;
+        byte var4 = 30;
+        byte var5 = 127;
+        //byte var6 = 128; //Compile 에러
+
 
         System.out.println("var1 = " + var1);
         System.out.println("var2 = " + var2);
         System.out.println("var3 = " + var3);
         System.out.println("var4 = " + var4);
-
+        System.out.println("var5 = " + var5);
     }
 
 }
--- src/chapter_2/sec_2/LongExample.java
+++ src/chapter_2/sec_2/LongExample.java
@@ -1,15 +1,15 @@
 package chapter_2.sec_2;
 
-public class IntegerLiteralExample {
+public class LongExample {
     public static void main(String[] args) {
-        int var1 = 0b1011;
-        int var2 = 0206;
-        int var3 = 365;
-        int var4 = 0xb3;
+        long var1 = 10;
+        long var2 = 20L;
+        //long var3 =10000000000; //타입이 맞지 않아 에러 발생
+        long var4 = 1000000000L;
 
         System.out.println("var1 = " + var1);
         System.out.println("var2 = " + var2);
-        System.out.println("var3 = " + var3);
+        //System.out.println("var3 = " + var3);
         System.out.println("var4 = " + var4);
 
     }
