69df566f59832f4d71cd0bf37dadd9d0a125dc36
seungjin-oh-kurly <seungjin.oh@kurlycorp.com>
2023-08-10 15:55:02 +0900

    chapter5 studied

---
+++ src/chapter_4_5/chapter_5/sec_3/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package chapter_4_5.chapter_5.sec_3;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        int[] intArray = null;
+        //intArray[0] = 10;
+
+        String str = null;
+        //System.out.println("str = " + str.length());
+
+    }
+}
